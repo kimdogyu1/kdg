70664a6b14c6de777b4beccedc8efe4d71217217
kimdogyu1 <47593270+kimdogyu1@users.noreply.github.com>
2019-06-27 17:01:41 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,26 @@
+﻿  function backgroundset(self) {
+    var target = document.querySelector('.img-button');
+    if (target.dataset.mode === 'On') {
+      target.style.backgroundImage = 'url(images/Moon.jpg)';
+      target.dataset.mode = 'Off';
+    }
+    else {
+      target.style.backgroundImage = 'url(images/sun.jpg)';
+      target.dataset.mode = 'On';
+    }
+  }
+  function Switch(self){
+    var target = document.querySelector('body');
+    if (target.dataset.mode === 'On'){
+      target.style.background = "linear-gradient(180deg, black, orange)";
+      target.dataset.mode = 'Off';
+      // backgroundset(1);
+      // self.value = 'Off';
+    }
+    else {
+      target.style.backgroundImage = 'none';
+      target.style.backgroundColor = 'black';
+      target.dataset.mode = 'On';
+      // self.value = 'On';
+    }
+  }
